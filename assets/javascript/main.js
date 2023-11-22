445f9661c7b8d6fbba2cc44fec22d5dd857d0330
hoangtan22th <hoangtan22th@gmail.com>
2023-11-22 18:10:53 +0700

    first commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -75,33 +75,51 @@ function changPage(i) {
 
 /// click logo quay ve trang chu 
 document.querySelector('svg').addEventListener('click',()=>{
-  window.location.href="./index.html"
+  window.location.href=''
 })
 
 // nut home load laij trang web
 var home = document.querySelector('#home')
 home.addEventListener('click',function(){
-  window.location.href = './index.html'
+  window.location.href = '/index.html'
 })
 
 //// nút đăng nhập 
 var dangNhap = document.querySelector('.button-login');
 dangNhap.addEventListener('click',function(){
-  window.location.href = './Login.html'
+  window.location.href = '/Login.html'
 })
 
 /// nut dat ngay
 var datNgay = document.querySelector('.button-sec1');
 datNgay.addEventListener('click',()=>{
-  window.location.href = './order.html'
+  window.location.href = '/order.html'
 })
 
 // nut xem menu 
 var xemMenu = document.querySelector('.button-sec2');
 xemMenu.addEventListener('click',()=>{
-  window.location.href = './menu.html'
+  window.location.href = '/menu.html'
 })
 
+// menu cake
+var menuCake = document.querySelector('#cake');
+menuCake.addEventListener('click',()=>{
+  window.location.href = '/menu.html'
+})
 
+//contact 
+var contact= document.querySelector('#contact')
+var teleContact =document.querySelector('.section-ten')
+contact.addEventListener('click',function(){
+  teleContact.scrollIntoView({behavior:"smooth"})
+})
+
+// about
+var about= document.querySelector('#about')
+var teleAbout =document.querySelector('.footer')
+about.addEventListener('click',function(){
+  teleAbout.scrollIntoView({behavior:"smooth"})
+})
 
  
