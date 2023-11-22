64e46e72631b86ab0c5cb866e0299d902f1d479f
hoangtan22th <hoangtan22th@gmail.com>
2023-11-22 17:10:11 +0700

    upd

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+// next page menu 
+let thisPage = 1;
+let limit = 6;
+let list = document.querySelectorAll(".list .item");
+function loadItem() {
+  let beginGet = limit * (thisPage - 1);
+  let endGet = limit * thisPage - 1;
+  list.forEach((item, key) => {
+    if (key >= beginGet && key <= endGet) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+  listPage();
+}
+loadItem();
+function listPage() {
+    let count = Math.ceil(list.length / limit);
+    document.querySelector(".listPage").innerHTML = "";
+  
+    if (thisPage > 1) {
+      let prev = document.createElement("li");
+      prev.innerText = "<";
+      prev.setAttribute("onclick", "changPage(" + (thisPage - 1) + ")");
+      document.querySelector(".listPage").appendChild(prev);
+    }
+  
+    let newPage = document.createElement("li");
+    newPage.innerHTML = thisPage;
+    newPage.classList.add("active");
+    document.querySelector(".listPage").appendChild(newPage);
+  
+    if (thisPage < count) {
+      let next = document.createElement("li");
+      next.innerText = ">";
+      next.setAttribute("onclick", "changPage(" + (thisPage + 1) + ")");
+      document.querySelector(".listPage").appendChild(next);
+    }
+  }
+  
+
+function changPage(i) {
+  thisPage = i;
+  loadItem();
+  document.querySelector('.section-six .list').scrollIntoView({behavior : 'smooth'});
+  
+  
+}
 /// click logo quay ve trang chu 
 document.querySelector('svg').addEventListener('click',()=>{
   window.location.href="./index.html"
@@ -151,52 +200,5 @@ oderNow.addEventListener("click", toggleForm);
 oderClose.addEventListener("click", toggleForm);
 bookTable.addEventListener("click", toggleForm);
 
-let thisPage = 1;
-let limit = 6;
-let list = document.querySelectorAll(".list .item");
-function loadItem() {
-  let beginGet = limit * (thisPage - 1);
-  let endGet = limit * thisPage - 1;
-  list.forEach((item, key) => {
-    if (key >= beginGet && key <= endGet) {
-      item.style.display = "block";
-    } else {
-      item.style.display = "none";
-    }
-  });
-  listPage();
-}
-loadItem();
-function listPage() {
-    let count = Math.ceil(list.length / limit);
-    document.querySelector(".listPage").innerHTML = "";
-  
-    if (thisPage > 1) {
-      let prev = document.createElement("li");
-      prev.innerText = "<";
-      prev.setAttribute("onclick", "changPage(" + (thisPage - 1) + ")");
-      document.querySelector(".listPage").appendChild(prev);
-    }
-  
-    let newPage = document.createElement("li");
-    newPage.innerHTML = thisPage;
-    newPage.classList.add("active");
-    document.querySelector(".listPage").appendChild(newPage);
-  
-    if (thisPage < count) {
-      let next = document.createElement("li");
-      next.innerText = ">";
-      next.setAttribute("onclick", "changPage(" + (thisPage + 1) + ")");
-      document.querySelector(".listPage").appendChild(next);
-    }
-  }
-  
 
-function changPage(i) {
-  thisPage = i;
-  loadItem();
-  document.querySelector('.section-six .list').scrollIntoView({behavior : 'smooth'});
-  
-  
-}
  
